bcfe79860abb1dc2475f4e2afc02b5a93eec336e
Daulet Toleugazy <daulettoleugazy@MacBook-Air-Daulet.local>
2023-11-24 16:22:49 +0600

    web final ready

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function showPricePopup() {
+    document.getElementById('pricePopup').style.display = 'block';
+  }
+  
+  function closePricePopup() {
+    document.getElementById('pricePopup').style.display = 'none';
+  }
